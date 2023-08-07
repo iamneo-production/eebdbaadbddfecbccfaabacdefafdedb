64bc7ecc1bcd71d2d9e540a62c0fc35faa3fc10c
Iamneo <techadmin@iamneo.ai>
2023-08-07 13:40:39 +0000

    Backup Commit

---
+++ springapp/src/main/java/services/Booking.java
@@ -0,0 +1,24 @@
+package services;
+//
+//public class Booking{
+//    //	Enter code here...
+//    public double booking(int noOfPersons, int rates){
+//        double totalRate;
+//        totalRate = noOfPersons * rates;
+//        return totalRate;
+//    }
+//}
+//
+
+//package services;
+
+public class Booking{
+    //	Enter code here...
+    public double booking(int noOfPersons, int rates){
+        double totalRate;
+        totalRate = noOfPersons * rates;
+        return totalRate;
+    }
+}
+
+
