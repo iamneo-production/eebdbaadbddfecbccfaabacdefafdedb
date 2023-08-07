f778ee00f6dc7fd278403598a306dfcaec250c79
Iamneo <techadmin@iamneo.ai>
2023-08-07 07:10:33 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/examly/springapp/services/Booking.java
@@ -0,0 +1,11 @@
+package com.examly.springapp.services;
+
+public class Booking{
+    //	Enter code here...
+    public double booking(int noOfPersons, int rates){
+        double totalRate;
+        totalRate = noOfPersons * rates;
+        return totalRate;
+    }
+}
+
