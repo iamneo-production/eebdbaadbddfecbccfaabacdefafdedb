724ef19365c0074c9b08cf9e34366d0868d706c1
Iamneo <techadmin@iamneo.ai>
2023-08-07 10:56:50 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/services/FareCalculator.java
+++ springapp/src/main/java/com/examly/springapp/services/FareCalculator.java
@@ -67,4 +67,70 @@ public class FareCalculator extends Booking{
         double result = fareCalculator.book(mockFlight);
         assertEquals(6000, result, 0);
     }
+
+    @Test
+    public void testBookFlightRoundTrip() {
+        FareCalculator fareCalculator = new FareCalculator();
+        
+        Flight mockFlight = mock(Flight.class);
+        when(mockFlight.getNoOfPersons()).thenReturn(2);
+        when(mockFlight.getRates()).thenReturn(3000);
+        when(mockFlight.getTriptype()).thenReturn("round-trip");
+        when(mockFlight.getFrom()).thenReturn("A");
+        when(mockFlight.getTo()).thenReturn("B");
+
+        double result = fareCalculator.book(mockFlight);
+        assertEquals(12000, result, 0);
+    }
+
+    @Test
+    public void testBookBus() {
+        FareCalculator fareCalculator = new FareCalculator();
+        
+        Bus mockBus = mock(Bus.class);
+        when(mockBus.getNoOfPersons()).thenReturn(3);
+        when(mockBus.getRates()).thenReturn(1000);
+
+        double result = fareCalculator.book(mockBus);
+        assertEquals(3000, result, 0);
+    }
+
+    @Test
+    public void testBookHotelValidDateRange() {
+        FareCalculator fareCalculator = new FareCalculator();
+        
+        Hotel mockHotel = mock(Hotel.class);
+        when(mockHotel.getNoOfPersons()).thenReturn(2);
+        when(mockHotel.getRates()).thenReturn(1500);
+        when(mockHotel.getFromdate()).thenReturn(LocalDate.of(2023, 8, 1));
+        when(mockHotel.getTodate()).thenReturn(LocalDate.of(2023, 8, 3));
+
+        double result = fareCalculator.book(mockHotel);
+        assertEquals(3000, result, 0);
+    }
+
+    @Test
+    public void testBookHotelInvalidDateRange() {
+        FareCalculator fareCalculator = new FareCalculator();
+        
+        Hotel mockHotel = mock(Hotel.class);
+        when(mockHotel.getFromdate()).thenReturn(LocalDate.of(2023, 8, 3));
+        when(mockHotel.getTodate()).thenReturn(LocalDate.of(2023, 8, 1));
+
+        double result = fareCalculator.book(mockHotel);
+        assertEquals(0, result, 0);
+    }
+
+    @Test
+    public void testBookTrain() {
+        FareCalculator fareCalculator = new FareCalculator();
+        
+        Train mockTrain = mock(Train.class);
+        when(mockTrain.getNoOfPersons()).thenReturn(4);
+        when(mockTrain.getRates()).thenReturn(200);
+
+        double result = fareCalculator.book(mockTrain);
+        assertEquals(800, result, 0);
+    }
+
 }
--- springapp/src/test/java/com/examly/springapp/testing/TestBooking.java
+++ springapp/src/test/java/com/examly/springapp/testing/TestBooking.java
@@ -15,21 +15,15 @@ public class TestBooking {
     @Test
     public void testBookingMethod() {
 
-        Booking booking = new Booking();
-
-        // Test cases
-        double result1 = booking.booking(2, 3000);
-        assertEquals(6000, result1, 0);
-        
-        // assertEquals(6000, book.booking(2, 3000),0);
-        // assertEquals(1000, book.booking(1, 1000),0);
-        // try {
-        //     book.booking(0, 5000);
-        //     book.booking(5, 0);
-        //     book.booking(0, 0);
-        // } catch (Exception e) {
-        //     e.printStackTrace();
-        // }
+        assertEquals(6000, book.booking(2, 3000),0);
+        assertEquals(1000, book.booking(1, 1000),0);
+        try {
+            book.booking(0, 5000);
+            book.booking(5, 0);
+            book.booking(0, 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
